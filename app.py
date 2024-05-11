2ace6d4849884ec0f0fb3cd2d550061cbef74a0d
Atikarn KRUAYKRIANGKRAI <atikarn.kr@ku.th>
2024-05-11 16:35:15 +0700

    add exit in menu

--- app.py
+++ app.py
@@ -29,6 +29,13 @@ class AcademicPerformanceManagementApp(tk.Tk):
 
         self.create_student_data_table()
         self.create_buttons()
+        menu = tk.Menu(self)
+        self.config(menu=menu)
+
+        file_menu = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="Import Data", command=self.import_data)
+        file_menu.add_command(label="Exit", command=self.quit)
 
         self.bind("<Configure>", self.resize)
 
